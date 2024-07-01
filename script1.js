f18a37d1dc0b338e4af336f89b9848941f7db3d5
Nishand-RM <nishandrm9@gmail.com>
2024-07-01 21:49:04 +0530

    script1.js

--- script1.js
+++ script1.js
@@ -4,3 +4,22 @@ const odd = [];
 arr.forEach((num) => num % 2 === 1 && odd.push(num));
 console.log(odd);
 
+//prime numbers
+let prime = (num) => {
+  if(num >2)
+  {
+    for(var i=2;i<num-1;i++)
+    {
+      if(num % i == 0)
+      {
+        console.log("not prime number");
+      }
+      else
+      {
+        console.log("prime number");
+      }
+    }
+  }
+  return num;
+}
+  console.log(prime(7));
